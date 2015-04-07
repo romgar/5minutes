5f525cb6cfb9fc4ecbffaf2e4ab2bb21beea7828
Natsu- <romain.garrigues.cs@gmail.com>
2015-04-07 21:42:47 +0100

    Updating fake links on pelicanconf

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,12 +22,11 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'))
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('@jrmi', 'https://twitter.com/jrmipp/'),
+          ('@rgarrigues', 'https://twitter.com/rgarrigues/'),)
 
 DEFAULT_PAGINATION = 10
 
