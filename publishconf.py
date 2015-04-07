cee360c08cde717110dcaf266ce3ba58e1edf853
Natsu- <romain.garrigues.cs@gmail.com>
2015-04-07 23:32:05 +0100

    Updating publishconf, according to new GitHub repo name

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://romgar.github.io/openblog'
+SITEURL = 'http://romgar.github.io/5minutes'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
