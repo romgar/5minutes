af14ca3995032234246ec45227814d5efdaefd46
Natsu- <romain.garrigues.cs@gmail.com>
2015-04-07 21:43:12 +0100

    Add CNAME file on extra folder for deployment

--- publishconf.py
+++ publishconf.py
@@ -18,6 +18,9 @@ CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
+STATIC_PATHS = ['extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
